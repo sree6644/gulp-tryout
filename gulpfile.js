a11094bd533d8ed561d32eabe4eb6ba2dc73fb13
sree6644 <sree6644@gmail.com>
2015-10-17 17:23:27 -0600

    added more

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-var gulp= require('gulp'),
-	gutil=require('gulp-util');
-	
-gulp.task('default',function(){
-	return gutil.log('hello sriram v2 with local git config');	
+var gulp = require('gulp'),
+	gutil = require('gulp-util');
+
+gulp.task('default', function () {
+	return gutil.log('hello sriram v2 with local git config');
+});
+
+gulp.task('maintask', function () {
+	return gutil.log('main task');
+});
+
+
+gulp.task('subtask', ['maintask'], function () {
+	return gutil.log('depedentent task');
 });
