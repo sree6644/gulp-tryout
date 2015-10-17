22b8e67f5fab233f5e8a81de28d882e01163f07b
sree6644 <sree6644@gmail.com>
2015-10-17 08:10:49 -0600

    new checkin with right id

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp= require('gulp'),
 	gutil=require('gulp-util');
 	
 gulp.task('default',function(){
-	return gutil.log('hello sriram');	
+	return gutil.log('hello sriram v2 with local git config');	
 });
